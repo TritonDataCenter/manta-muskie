3db7e049de4146788a4d212509cb4b0d224e316c
Kody A Kantor <kody@kkantor.com>
2018-04-26 19:38:06 +0000

    MANTA-3523 make muskie monitoring tests optional, improve port wrangling

--- test/monitoring.test.js
+++ test/monitoring.test.js
@@ -14,6 +14,7 @@ var helper = require('./helper.js');
 
 var restify = require('restify');
 var url = require('url');
+var net = require('net');
 var jsprim = require('jsprim');
 
 var before = helper.before;
@@ -21,30 +22,57 @@ var test = helper.test;
 
 ///--- Tests
 
+
 before(function (cb) {
     this.kangPath = '/kang/snapshot';
     this.metricsPath = '/metrics';
+
+    var port;
 
     /*
      * Since the monitoring server runs on a separate port (http_port + 800),
      * we need to do some slicing and dicing of the user-provided MANTA_URL.
      *
-     * Note: This requires that the user follows the README and set MANTA_URL
-     * to point to a running Muskie instance. This will fail if MANTA_URL
+     * Note: To run the monitoring tests, the user must set MANTA_URL
+     * to point to a running Muskie instance. The tests will fail if MANTA_URL
      * points to a loadbalancer, or doesn't include a port number.
+     *
+     * The MANTA_URL needs to have an IP address and port number. It could still
+     * be the case that the MANTA_URL points to a loadbalancer's IP and port,
+     * which will cause the monitoring tests to fail.
      */
 
     var parsed_url = url.parse(process.env.MANTA_URL);
     if (typeof (parsed_url.port) !== 'string') {
-        cb(new Error('MANTA_URL must include a valid port number'));
+        this.skip = true;
+        console.log('skipping monitoring tests: port number not found on' +
+            ' MANTA_URL');
+        cb();
+        return;
+    }
+
+    /*
+     * If the hostname isn't an IP then we're _probably_ not pointing at a
+     * muskie process.
+     */
+    if (! net.isIP(parsed_url.hostname) &&
+            parsed_url.hostname !== 'localhost') {
+
+        this.skip = true;
+        console.log('skipping monitoring tests: MANTA_URL is not an IP' +
+                ' address and port');
+        cb();
         return;
     }
 
     // Take the :port section of the URL and add 800.
-    var port = jsprim.parseInteger(parsed_url.port);
+    port = jsprim.parseInteger(parsed_url.port);
     if (typeof (port) !== 'number') {
         // parseInteger() returned an error, not a number.
-        cb(new Error('error parsing MANTA_URL port: ' +  port.message));
+        this.skip = true;
+        console.log('skipping monitoring test: error parsing port number on' +
+                ' MANTA_URL');
+        cb();
         return;
     }
     port += 800;
@@ -59,6 +87,10 @@ before(function (cb) {
 });
 
 test('kang handler running', function (t) {
+    if (this.skip) {
+        t.end();
+        return;
+    }
     var client = restify.createJsonClient({
         connectTimeout: 250,
         rejectUnauthorized: false,
@@ -75,6 +107,10 @@ test('kang handler running', function (t) {
 });
 
 test('metric handler running', function (t) {
+    if (this.skip) {
+        t.end();
+        return;
+    }
     var client = restify.createStringClient({
         connectTimeout: 250,
         rejectUnauthorized: false,
